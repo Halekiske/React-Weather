0f2be8d602491e2f863a9ce878167c66c357d5c5
Mohammed Sahi <halekiske@gmail.com>
2023-10-30 16:44:08 +0800

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6e85391449msh1dcecbbbc5314e4p1bf087jsn6dda077906d8",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f9184857c8ac8bc0c48831fc5b47b9fe";
